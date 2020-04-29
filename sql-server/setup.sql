ad7b86f01411e60f4552f9ff6bc7f91de6fcd262
Seth Sacher <ssacher@collabralink.com>
2020-04-29 18:28:28 -0400

    Restore SQL Server from backup (requires local copy of bak file)

--- sql-server/setup.sql
+++ sql-server/setup.sql
@@ -1,2 +1,6 @@
-RESTORE DATABASE SCHIPAnnualReports FROM DISK='/var/opt/mssql/backup/SCHIPAnnualReports_2019-12-19.bak';
+RESTORE DATABASE SCHIPAnnualReports
+FROM DISK = '/var/opt/mssql/backup/SCHIPAnnualReports_2019-12-19.bak'
+WITH MOVE 'SCHIPAnnualReports_dat' TO '/var/opt/mssql/data/SCHIPAnnualReports.mdf',
+MOVE 'SCHIPAnnualReports_Index' TO '/var/opt/mssql/data/SCHIPAnnualReports_Index.ndf',
+MOVE 'SCHIPAnnualReports_log' TO '/var/opt/mssql/data/SCHIPAnnualReports_Log.ldf'
 GO
